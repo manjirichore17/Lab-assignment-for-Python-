b24efd431f955709e7af61ae9900e78f2a48699b
manjirichore17 <manjirichore247@gmail.com>
2026-03-09 23:25:05 +0530

    Add files via upload

---
+++ pract3b.py
@@ -0,0 +1,13 @@
+n = int(input("Enter the number of times to print:"))
+
+print("Using while loop:")
+i = 0
+while (i < n):
+    print("Hello Students")
+    i += 1
+
+
+# Using for loop
+print("Using for loop:")
+for i in range(n):
+    print("Hello Students")
